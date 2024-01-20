037bd86f7c534c1b5160149f90569797ca895cd5
chrhuncho <cooper.chris@student.oaklandcc.edu>
2024-01-20 15:23:07 -0500

    lab 1

---
+++ pythonProject/main.py
@@ -0,0 +1,4 @@
+print('Chris Cooper he/him')
+print('I am taking this class because of a requirement, and enjoy programming')
+print('Chris is usually playing video games, at the gym, cooking, or enjoying his favorite shows/movies')
+print('Chris is a huge history nerd and could tell you about almost any era')
