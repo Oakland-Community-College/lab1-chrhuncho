75e4341167658b85ad578ccd61c69da035fcf8e1
chrhuncho <cooper.chris@student.oaklandcc.edu>
2024-01-28 19:38:17 -0500

    Project 1 Week 3

--- pythonProjectWeek3/main.py
+++ pythonProjectWeek3/main.py
@@ -1,11 +1,34 @@
-print('Loading Fence Calculating Program.........')
-print('Welcome to Fence Calcualtor Wizard')
+#Project 1
+#Chris Cooper
+
+
+
+print('Welcome to Fence Calculator Wizard')
 print('Lets start with the dimensions of your fence')
-#total fence distance is length x width
-length = float(input("What is the length of your fence?(in feet)\n"))
-width = float(input("What is the width of your fence?(in inches)\n"))
+#function to check user input
+def check_int(prompt):
+    user_input = input(prompt)
+    while(True):
+        try:
+            user_input = int(user_input)
+        except:
+            print ("Input not recognized. Please enter an integer")
+            user_input = input(prompt)
+        if user_input < 0:
+            print ("Cannot have a negative dimension. Please enter an integer.")
+            user_input = input(prompt)
+        else:
+            return user_input
+
+#user puts information such as length, width, and cost of fence
+length = check_int("What is the length of your fence in feet?\n")
+width = float(input("What is the width of your fence in feet?\n"))
 post_distance = float(input("How far apart (in inches) do you want your posts to be?\n"))
 print('Ok, can we now calculate how much of supplies you will need')
-post_number=length * width
-print("Fence Distance is",post_number)
+#user chooses how much of fence boarding to set price
+boards = float(input("Please choose how many boards you will need:\n"))
+#calcuating cost of fence
+price = float(input("Please enter price of fence per foot\n"))
+print ("The total cost of your fence is $" + str(boards*(length + width * post_distance)*price) + ".")
+print('Thank you for using Fence Calculator Wizard')
 
