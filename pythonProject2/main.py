817a1e6e08a70a2be5933bc2b8735523b6b77cfa
chrhuncho <cooper.chris@student.oaklandcc.edu>
2024-02-07 14:48:41 -0500

    Lab 4 Functions

---
+++ pythonProject2/main.py
@@ -0,0 +1,39 @@
+#Chris Cooper
+#Lab 4 - Function
+import random
+import math
+#User will create a random # generator, add numbers such as 1 through 5.
+def affirmation_generator():
+    number = random.randint(1, 5)
+    if number == 1:
+        print("Everything is going to be ok. Keep up the good work.")
+    elif number == 2:
+        print("Hard times never last forever.")
+    elif number == 3:
+        print("I am liberating myself from fear, judgment, and doubt.")
+    elif number == 4:
+        print("My body is healthy, my mind is brilliant, and my soul is tranquil.")
+    else:
+        print("My time is just around the corner.")
+affirmation_generator()
+affirmation_generator()
+affirmation_generator()
+affirmation_generator()
+affirmation_generator()
+
+#Lab 4 - Quadratic Function Calculator
+#Input quadratic equation information such as values.
+def quadratic_equation_intercept_finder(a ,b ,c):
+    determinant =b**2 - 4 * a * c
+    if determinant < 0:
+        return "No intercepts have been found."
+    if determinant > 0:
+        return "Intercepts have been found."
+
+    first_x_intercept =(-b + math.sqrt(determinant)) / (2 * a)
+    second_x_intercept = (-b - math.sqrt(determinant)) / (2 * a)
+
+    return "x intercepts of " + str(first_x_intercept) + " and " + str(second_x_intercept)
+#User should enter numbers in QEIF
+result = quadratic_equation_intercept_finder(2, 4 ,6)
+print(result)
